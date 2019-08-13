e02bdacad65e474486a87351be7ae2842a3c54bc
kevin Abreu <kabreu11385@gmail.com>
2019-08-13 22:13:07 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,6 +5,7 @@ var playlist = {
 }
 
 function updatePlaylist(obj, key, value) {
-    return Object.playlist({}),Object,{[key]}: value})
+    return Object.assign({}),
+    obj, { [key]: value })
   
 }
